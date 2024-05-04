6fada41a53cc88a0044e7942a5259693e5c97aea
Viktor <167081310+DrVikt@users.noreply.github.com>
2024-05-04 21:57:18 +0400

    Add files via upload

---
+++ Lessons/Class1/2_9.py
@@ -0,0 +1,9 @@
+#Подвиг 11. Напишите программу вывода переменной math.pi с точностью до тысячных (три знака после запятой).
+#Sample Input:
+#Sample Output:
+#3.142
+
+
+# put your python code here
+import math
+print(round(math.pi, 3))
